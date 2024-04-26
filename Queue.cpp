895a13d0485ac5abea70b3ffb85626a7c7858ce1
chen-huaneng <huanengchen@foxmail.com>
2024-04-26 11:46:25 +0800

    add some queue button.

--- Queue.cpp
+++ Queue.cpp
@@ -28,7 +28,16 @@ void Queue::DoDataExchange(CDataExchange* pDX)
 
 
 BEGIN_MESSAGE_MAP(Queue, CDialogEx)
+	ON_NOTIFY(LVN_ITEMCHANGED, IDC_LIST1, &Queue::OnLvnItemchangedList1)
 END_MESSAGE_MAP()
 
 
 // Queue 消息处理程序
+
+
+void Queue::OnLvnItemchangedList1(NMHDR* pNMHDR, LRESULT* pResult)
+{
+	LPNMLISTVIEW pNMLV = reinterpret_cast<LPNMLISTVIEW>(pNMHDR);
+	// TODO: 在此添加控件通知处理程序代码
+	*pResult = 0;
+}
