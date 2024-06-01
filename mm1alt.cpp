dcc7874c724d9447f3459b9212a8156bab931743
chen-huaneng <huanengchen@foxmail.com>
2024-06-01 17:54:39 +0800

    modify the mm1alt the maximum delay in queue and the proportion of customers having a delay in queue in excess of 1 minute.

--- mm1alt.cpp
+++ mm1alt.cpp
@@ -7,14 +7,14 @@ vector<float> mm1alt::mm1Alt(float m_l, float m_s, float t_e, float d_e) /* Main
 {
     /* Specify the number of events for the timing function. */
     
-    num_events = 3;
+    num_events = 3; // 事件数
 
     /* Read input parameters. */
 
-	mean_interarrival = m_l;
-	mean_service = m_s;
-    time_end = t_e;
-	delay_excess = d_e;
+	mean_interarrival = m_l; // 平均到达时间
+	mean_service = m_s; // 平均服务时间
+    time_end = t_e; // 仿真结束时间
+	delay_excess = d_e; // 延迟时间
 
     /* Initialize the simulation. */
 
@@ -62,17 +62,17 @@ void mm1alt::initialize() /* Initialization function. */
 {
 	/* Initialize the results vector. */
 
-    results.clear();
+    results.clear(); // 初始化结果向量
 
     /* Initialize the simulation clock. */
 
-    sim_time = 0.0;
+    sim_time = 0.0; // 初始化仿真时间
 
     /* Initialize the state variables. */
 
-    server_status = IDLE;
-    num_in_q = 0;
-    time_last_event = 0.0;
+    server_status = IDLE; // 初始化服务器状态
+    num_in_q = 0; // 初始化队列中的人数
+    time_last_event = 0.0; // 初始化上一个事件的时间
 
 	// 初始化系统中的人数和总时间
 
@@ -82,24 +82,24 @@ void mm1alt::initialize() /* Initialization function. */
 
     /* Initialize the statistical counters. */
 
-    num_custs_delayed = 0;
-    total_of_delays = 0.0;
-    area_num_in_q = 0.0;
+    num_custs_delayed = 0; // 初始化延迟的顾客数
+    total_of_delays = 0.0; // 初始化延迟总时间
+    area_num_in_q = 0.0; //
     area_server_status = 0.0;
 
 	// 初始化系统中的人数和总时间
 
     total_num_in_system = 0.0; // 初始化系统中的人数
     total_time_in_system = 0.0; // 初始化系统中的总时间
-	num_custs_delayed_over_1_min = 0; // 初始化计数器
+	num_custs_delayed_over_1_min = 0; // 初始化延迟超过给定时间的顾客数
 
     /* Initialize event list.  Since no customers are present, the departure
        (service completion) event is eliminated from consideration.  The end-
        simulation event (type 3) is scheduled for time time_end. */
 
-    time_next_event[1] = sim_time + expon(mean_interarrival);
-    time_next_event[2] = 1.0e+30;
-	time_next_event[3] = time_end;
+    time_next_event[1] = sim_time + expon(mean_interarrival); // 初始化下一个到达时间
+    time_next_event[2] = 1.0e+30; // 初始化离开时间
+	time_next_event[3] = time_end; // 初始化仿真结束时间
 }
 
 int mm1alt::timing(void) /* Timing function. */
@@ -192,18 +192,6 @@ int mm1alt::arrive(void) /* Arrival event function. */
         ++num_custs_delayed;
         server_status = BUSY;
 
-		// 在顾客开始服务时计算延迟并更新最大延迟变量
-        delay = sim_time - time_arrival[1];
-        if (delay > max_delay) {
-            max_delay = delay;
-        }
-
-        // 如果顾客延迟时间超过一分钟
-		if (delay > delay_excess) {
-			++num_custs_delayed_over_1_min;
-		}
-        delay = 0.0;
-
         /* Schedule a departure (service completion). */
 
         time_next_event[2] = sim_time + expon(mean_service);
@@ -226,7 +214,6 @@ void mm1alt::depart(void) /* Departure event function. */
         server_status = IDLE;
         time_next_event[2] = 1.0e+30;
     }
-
     else
     {
         /* The queue is nonempty, so decrement the number of customers in
@@ -240,10 +227,15 @@ void mm1alt::depart(void) /* Departure event function. */
         delay = sim_time - time_arrival[1];
         total_of_delays += delay;
 
-		 // 在顾客离开时计算延迟并更新最大延迟变量
+		// 在顾客离开时计算延迟并更新最大延迟变量
         if (delay > max_delay) {
             max_delay = delay;
         }
+
+        // 如果顾客延迟时间超过一分钟
+		if (delay > delay_excess) {
+			++num_custs_delayed_over_1_min;
+		}
 
         /* Increment the number of customers delayed, and schedule departure. */
 
@@ -261,14 +253,10 @@ void mm1alt::depart(void) /* Departure event function. */
 void mm1alt::report(void) /* Report generator function. */
 {
     /* Compute and write estimates of desired measures of performance. */
-	//results.push_back(mean_interarrival);
-	//results.push_back(mean_service);
-	//results.push_back(num_delays_required);
-
-	results.push_back(total_of_delays / num_custs_delayed);
-	results.push_back(area_num_in_q / sim_time);
-	results.push_back(area_server_status / sim_time);
-	results.push_back(num_custs_delayed);
+	results.push_back(total_of_delays / num_custs_delayed); // 计算 time average delay of customers in queue
+	results.push_back(area_num_in_q / sim_time); // 计算 average number of customers in queue
+	results.push_back(area_server_status / sim_time); // 计算 server utilization
+	results.push_back(num_custs_delayed); // 计算 number of customers delayed
 
     results.push_back(total_num_in_system / sim_time); // 计算系统中的平均人数
     results.push_back(total_time_in_system / num_custs_delayed); // 计算顾客在系统中的平均总时间
@@ -276,6 +264,7 @@ void mm1alt::report(void) /* Report generator function. */
     results.push_back(max_delay);    // 将最大延迟写入结果向量
     results.push_back(max_time_in_system); // 写入最大系统时间
 
+    // 计算延迟超过给定时间的顾客比例
 	float proportion_delayed_over_1_min = (float)num_custs_delayed_over_1_min / num_custs_delayed;
 	results.push_back(proportion_delayed_over_1_min);
 }
